62e634a0d6d7356bacee5131b2ed4b4fd0b160be
KELOHL21 <kelsyohlsson@gmail.com>
2022-06-09 11:17:46 +0200

    Changes

---
+++ Src/includes/nav.inc.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Stylesheet/home_stylesheet.css"> 
+    <script src="https://kit.fontawesome.com/45c101dd7d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+<header>
+ <!--Logo Image needs to come there-->
+  <h1 class="logo">Logo</h1>
+
+  <input type="checkbox" id="nav_toggle" class="nav_toggle">
+
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Contact</a></li>
+      <input type="text" id="search_bar" class="search_bar"/></input>
+    </ul>
+    
+  </nav>
+
+  <label for="nav_toggle" class="nav_toggle_label">
+    <span><i class="fa-solid fa-bars fa-lg"></i></span>
+  </label>
+
+</header>
+
+<br>
+
+</body>
+</html>
--- home.php
+++ home.php
@@ -7,27 +7,28 @@
     <title>Home Page</title>
 
     <link rel="stylesheet" href="./Src/Stylesheet/home_stylesheet.css">  
-
     <script src="https://kit.fontawesome.com/e4ad388285.js" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <nav>
-    
-     </nav>
 
-    <container class="Header">
+         <?php
+         include("./src/includes/nav.inc.php")
+         ?>
+
+    <container class="intro">
 
     <div class="welcome_message">
-    
+
     <?php
          //convert into session
-        $firstname = $_POST["firstName"];
-        $email = $_POST["email"];
+           $firstname = $_POST["firstName"];
+           $email = $_POST["email"];
 
-          echo '<h1>Welcome</h1>'
-           .$firstname;
-           echo '<br>';
-          echo "<p></p>";
+          echo '<h1>Welcome</h1>'.$firstname;
+          echo '<br>';
+          echo "<p>Cape Town the MotherCity. Its vibrant colourful and adventurous.Everywhere you go you'll be amazed.
+          Choose from a variety of hotels to enjoy your stay.As they say in Cape 'Heres iet lekker'. </p>";
 
      ?>
     </div>
@@ -35,16 +36,23 @@
      <div class="info_required">
         <h1 >Information Required</h1>
 
-        <form class="info_form"action="" method="post">
+        <form class="info_form" action="./home.php" method="post">
 
-        <label for="days staying">How long will you be staying?</label>
+        <div class="data">
+        <label for="days staying">Days staying?</label>
         <input type="number" name="days_stayed">
+        </div>
 
+        <div class="data">
         <label for="dates">From:</label>
         <input type="date" name="from_date">
+        </div>
 
+        <div class="data">
         <label for="dates">To:</label>
         <input type="date" name="to_date">
+        </div>
+
         <input type="submit">
 
         </form>
@@ -52,7 +60,5 @@
 
     </container>
 
-
- 
 </body>
 </html>
